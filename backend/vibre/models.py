985860e0751b2f1063d2fc47ec7932a08c3c30b6
MarcosDMT <marcosgav80@gmail.com>
2022-11-17 11:50:25 +0300

    created a frontend folder

--- backend/vibre/models.py
+++ backend/vibre/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
+    image = models.ImageField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
